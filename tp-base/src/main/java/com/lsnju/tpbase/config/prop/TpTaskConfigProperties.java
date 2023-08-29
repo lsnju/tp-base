0e9d28ef490dfae935d6d892fbf9ca9b89952501
lisong <lisong@lsnju.com>
2023-08-29 09:51:47 +0800

    【 tp 】：update TpTaskAutoConfiguration

--- tp-base/src/main/java/com/lsnju/tpbase/config/prop/TpTaskConfigProperties.java
+++ tp-base/src/main/java/com/lsnju/tpbase/config/prop/TpTaskConfigProperties.java
@@ -16,8 +16,11 @@ import lombok.Setter;
 @ConfigurationProperties(prefix = "tp.task")
 public class TpTaskConfigProperties {
     private boolean enable;
-    private String tpMoCron;
-    private String cpMoCron;
-    private String commonErrorCron;
+    private String tpMoCron = "*/5 * * * * ?";
+    private String cpMoCron = "*/5 * * * * ?";
+    private String commonErrorCron = "5 1 0 * * ?";
+    private boolean tpMoEnable = true;
+    private boolean cpMoEnable = true;
+    private boolean commonErrorEnable = false;
     private boolean scheduler;
 }
