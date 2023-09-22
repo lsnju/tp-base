29e8c315930129b97700a8b13127478b96d0d5b4
lisong <lisong@lsnju.com>
2023-09-22 11:09:51 +0800

    【 v2 】：aop log

---
+++ tp-base/src/main/java/com/lsnju/tpbase/config/prop/TpAopConfigProperties.java
@@ -0,0 +1,21 @@
+package com.lsnju.tpbase.config.prop;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import com.lsnju.base.model.BaseMo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author ls
+ * @since 2023/9/22 10:46
+ * @version V1.0
+ */
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "tp.aop")
+public class TpAopConfigProperties extends BaseMo {
+    private boolean useSpring;
+}
