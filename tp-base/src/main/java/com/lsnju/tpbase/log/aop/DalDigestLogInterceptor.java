808026fbc7647e11e958f3817bac12fec42c253b
lisong <lisong@lsnju.com>
2024-12-12 17:56:56 +0800

    refactor

--- tp-base/src/main/java/com/lsnju/tpbase/log/aop/DalDigestLogInterceptor.java
+++ tp-base/src/main/java/com/lsnju/tpbase/log/aop/DalDigestLogInterceptor.java
@@ -1,18 +1,9 @@
 package com.lsnju.tpbase.log.aop;
 
-import java.lang.reflect.Method;
-
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.aop.framework.AopProxyUtils;
 
-import com.lsnju.base.util.Profiler;
-import com.lsnju.tpbase.config.prop.TpAopConfigProperties;
 import com.lsnju.tpbase.log.DigestConstants;
-import com.lsnju.tpbase.util.TpAopUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,57 +15,14 @@ import lombok.extern.slf4j.Slf4j;
  * @version V1.0
  */
 @Slf4j
-public class DalDigestLogInterceptor implements MethodInterceptor, DigestConstants {
+public class DalDigestLogInterceptor extends AbstractDigestLogInterceptor {
 
     /** 摘要日志存储文件 */
     protected static final Logger digestLogger = LoggerFactory.getLogger(DigestConstants.TP_DAL_DIGEST);
 
-    // [接口.方法名,耗时,成功失败]
-    /** 日志格式 */
-    protected static final String FORMAT_STR = "[%s.%s,%sms,%s]";
-
-    private final TpAopConfigProperties tpAopConfigProperties;
-
-    public DalDigestLogInterceptor(TpAopConfigProperties tpAopConfigProperties) {
-        this.tpAopConfigProperties = tpAopConfigProperties;
-    }
-
     @Override
-    public Object invoke(MethodInvocation invocation) throws Throwable {
-        log.debug("{}", invocation);
-
-        Method method = invocation.getMethod();
-        String className = getClassName(invocation);
-        String methodName = method.getName();
-
-        String code = "S";
-        long startTime = System.nanoTime();
-        try {
-            String argDesc = TpAopUtils.argumentsDesc(invocation);
-            if (StringUtils.isNotBlank(argDesc)) {
-                Profiler.enter(String.format("%s.%s %s", className, methodName, argDesc));
-            } else {
-                Profiler.enter(String.format("%s.%s", className, methodName));
-            }
-            return invocation.proceed();
-        } catch (Throwable e) {
-            code = "E";
-            throw e;
-        } finally {
-            Profiler.release();
-            if (digestLogger.isInfoEnabled()) {
-                digestLogger.info(String.format(FORMAT_STR, className, methodName, (System.nanoTime() - startTime) / MS_SCALE, code));
-            }
-        }
-    }
-
-    private String getClassName(MethodInvocation invocation) {
-        if (tpAopConfigProperties.isUseSpring() && invocation.getThis() != null) {
-            Class<?>[] classes = AopProxyUtils.proxiedUserInterfaces(invocation.getThis());
-            return classes[0].getSimpleName();
-        } else {
-            return invocation.getMethod().getDeclaringClass().getSimpleName();
-        }
+    Logger digestLogger() {
+        return digestLogger;
     }
 
 }
