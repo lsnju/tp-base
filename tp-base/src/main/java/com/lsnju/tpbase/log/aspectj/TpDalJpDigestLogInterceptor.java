0a738880b5acff46c5d8cd4d08556db93141882a
lisong <lisong@lsnju.com>
2024-12-14 12:57:37 +0800

    add Aspectj support

---
+++ tp-base/src/main/java/com/lsnju/tpbase/log/aspectj/TpDalJpDigestLogInterceptor.java
@@ -0,0 +1,24 @@
+package com.lsnju.tpbase.log.aspectj;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.lsnju.tpbase.log.DigestConstants;
+
+/**
+ *
+ * @author ls
+ * @since 2024/5/28 12:52
+ * @version V1.0
+ */
+public class TpDalJpDigestLogInterceptor extends AbstractJpDigestLogInterceptor {
+
+    /** 摘要日志存储文件 */
+    protected static final Logger digestLogger = LoggerFactory.getLogger(DigestConstants.TP_DAL_DIGEST);
+
+    @Override
+    Logger digestLogger() {
+        return digestLogger;
+    }
+
+}
