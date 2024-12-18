8c9941c0ae96cd27875dee7f01e84ad4d13001c5
lisong <lisong@lsnju.com>
2024-12-18 11:00:02 +0800

    refactor restApiDigest log

--- tp-base/src/main/java/com/lsnju/tpbase/web/filter/rest/TpRestApiDigestFilter.java
+++ tp-base/src/main/java/com/lsnju/tpbase/web/filter/rest/TpRestApiDigestFilter.java
@@ -1,6 +1,7 @@
 package com.lsnju.tpbase.web.filter.rest;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -41,11 +42,19 @@ public class TpRestApiDigestFilter extends AbstractTpRequestFilter implements Di
             if (DIGEST_LOGGER.isInfoEnabled()) {
                 final long costTime = (System.nanoTime() - startTime) / DigestConstants.MS_SCALE;
                 final String msg = String.format(DIGEST_FORMAT, request.getMethod(), request.getServletPath(), costTime,
-                    TpRestContext.getRsCode(), TpRestContext.getRsMsg());
+                    getRsCode(), getRsMsg());
                 DIGEST_LOGGER.info(msg);
             }
             TpRestContext.clear();
         }
+    }
+
+    private static String getRsMsg() {
+        return Objects.toString(TpRestContext.getRsMsg(), "-");
+    }
+
+    private static String getRsCode() {
+        return Objects.toString(TpRestContext.getRsCode(), "-");
     }
 
 }
