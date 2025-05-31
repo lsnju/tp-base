7fa96e00eb8292bdbb0d1fa2658528e4576f85f5
lisong <lisong@lsnju.com>
2025-05-31 10:40:15 +0800

    polishing

--- tp-utils/src/fun-test/java/com/lsnju/base/util/TpDateFormatUtilsFunTest.java
+++ tp-utils/src/fun-test/java/com/lsnju/base/util/TpDateFormatUtilsFunTest.java
@@ -72,4 +72,30 @@ public class TpDateFormatUtilsFunTest {
         log.info("{}", DateTimeFormatter.BASIC_ISO_DATE.format(ZonedDateTime.now()));
         log.info("{}", DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now()));
     }
+
+    @Test
+    void test_003() {
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+            log.info("{}", simpleDateFormat.format(new Date()));
+            log.info("{}", simpleDateFormat.parse("2025-05-27T00:00:00.000+08:00"));
+        } catch (ParseException e) {
+            log.error(String.format("%s", e.getMessage()), e);
+        }
+    }
+
+    @Test
+    void test_004() {
+        Date now = new Date();
+        log.info("{}", DateFormatUtils.format(now, "yyyy-MM-dd'T'HH:mm:ss.SSSX"));
+        log.info("{}", DateFormatUtils.format(now, "yyyy-MM-dd'T'HH:mm:ss.SSSXX"));
+        log.info("{}", DateFormatUtils.format(now, "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"));
+//        log.info("{}", DateFormatUtils.format(now, "yyyy-MM-dd'T'HH:mm:ss.SSSXXXX"));
+        log.info("---");
+        log.info("{}", DateFormatUtils.format(now, "yyyy-MM-dd'T'HH:mm:ss.SSSZ"));
+        log.info("{}", DateFormatUtils.format(now, "yyyy-MM-dd'T'HH:mm:ss.SSSZZ"));
+        log.info("{}", DateFormatUtils.format(now, "yyyy-MM-dd'T'HH:mm:ss.SSSZZZ"));
+        log.info("{}", DateFormatUtils.format(now, "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZ"));
+    }
+
 }
