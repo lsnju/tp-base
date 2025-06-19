59e893c56fe5068c8f752a374ceebc3d07ca252f
lisong <lisong@lsnju.com>
2025-06-19 08:34:41 +0800

    update TpHttpClientUtils

--- tp-utils/src/main/java/com/lsnju/base/http/TpHttpClientUtils.java
+++ tp-utils/src/main/java/com/lsnju/base/http/TpHttpClientUtils.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.net.URI;
 
 import org.apache.hc.client5.http.fluent.Executor;
+import org.apache.hc.client5.http.fluent.Request;
 import org.apache.hc.core5.http.ClassicHttpResponse;
 import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpEntity;
 
 import com.lsnju.base.http.config.HttpConfig;
+import com.lsnju.base.http.config.HttpMethod;
 import com.lsnju.base.http.config.RequestCustomizer;
 import com.lsnju.base.http.impl.DefaultTpHttpClientImpl;
 
@@ -152,6 +155,64 @@ public class TpHttpClientUtils {
 
     public static ClassicHttpResponse get(URI targetUrl, RequestCustomizer customizer, Executor executor) throws IOException {
         return HTTP_CLIENT.get(targetUrl, customizer, executor);
+    }
+
+    // putJson
+
+    public static ClassicHttpResponse putJson(String targetUrl, String rawReq, Executor executor) throws IOException {
+        return HTTP_CLIENT.putJson(targetUrl, rawReq, executor);
+    }
+
+    public static ClassicHttpResponse putJson(URI targetUrl, String rawReq, Executor executor) throws IOException {
+        return HTTP_CLIENT.putJson(targetUrl, rawReq, executor);
+    }
+
+    public static ClassicHttpResponse putJson(String targetUrl, String rawReq) throws IOException {
+        return HTTP_CLIENT.putJson(targetUrl, rawReq);
+    }
+
+    public static ClassicHttpResponse putJson(URI targetUrl, String rawReq) throws IOException {
+        return HTTP_CLIENT.putJson(targetUrl, rawReq);
+    }
+
+    public static ClassicHttpResponse putJson(String targetUrl, String rawReq, RequestCustomizer customizer) throws IOException {
+        return HTTP_CLIENT.putJson(targetUrl, rawReq, customizer);
+    }
+
+    public static ClassicHttpResponse putJson(URI targetUrl, String rawReq, RequestCustomizer customizer) throws IOException {
+        return HTTP_CLIENT.putJson(targetUrl, rawReq, customizer);
+    }
+
+    public static ClassicHttpResponse putJson(String targetUrl, String rawReq, RequestCustomizer customizer, Executor executor) throws IOException {
+        return HTTP_CLIENT.putJson(targetUrl, rawReq, customizer, executor);
+    }
+
+    public static ClassicHttpResponse putJson(URI targetUrl, String rawReq, RequestCustomizer customizer, Executor executor) throws IOException {
+        return HTTP_CLIENT.putJson(targetUrl, rawReq, customizer, executor);
+    }
+
+    // http xxx
+
+    public static ClassicHttpResponse http(HttpMethod method, URI targetUrl, HttpEntity entity) throws IOException {
+        return HTTP_CLIENT.http(method, targetUrl, entity);
+    }
+
+    public static ClassicHttpResponse http(HttpMethod method, URI targetUrl, HttpEntity entity, RequestCustomizer customizer) throws IOException {
+        return HTTP_CLIENT.http(method, targetUrl, entity, customizer);
+    }
+
+    public static ClassicHttpResponse http(HttpMethod method, URI targetUrl, HttpEntity entity, Executor executor) throws IOException {
+        return HTTP_CLIENT.http(method, targetUrl, entity, executor);
+    }
+
+    public static ClassicHttpResponse http(HttpMethod method, URI targetUrl, HttpEntity entity, RequestCustomizer customizer, Executor executor) throws IOException {
+        return HTTP_CLIENT.http(method, targetUrl, entity, customizer, executor);
+    }
+
+    // http request
+
+    public static Request request(HttpMethod method, URI targetUrl) {
+        return HTTP_CLIENT.request(method, targetUrl);
     }
 
 }
