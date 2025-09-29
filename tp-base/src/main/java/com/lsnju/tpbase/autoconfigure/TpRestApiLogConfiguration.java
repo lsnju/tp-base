0dd29e66a798647bb0fe6ef445732417af63637c
lisong <ls_demon@qq.com>
2025-09-29 21:30:24 +0800

    add toggle for rest-api-log

--- tp-base/src/main/java/com/lsnju/tpbase/autoconfigure/TpRestApiLogConfiguration.java
+++ tp-base/src/main/java/com/lsnju/tpbase/autoconfigure/TpRestApiLogConfiguration.java
@@ -6,6 +6,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -26,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Aspect
 @Configuration
+@ConditionalOnProperty(name = "tp.aop.enable-rest-log", matchIfMissing = true)
 @Import(TpRestApiLogConfiguration.TpRestApiLogInterceptorConfig.class)
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class TpRestApiLogConfiguration {
