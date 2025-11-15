5cf8926f4cbc9a0898df1dbd37b021647fdcdad3
lisong <ls_demon@qq.com>
2025-11-15 21:54:01 +0800

    polishing

--- tp-base/src/main/java/com/lsnju/tpbase/test/TpPerformanceUtils.java
+++ tp-base/src/main/java/com/lsnju/tpbase/test/TpPerformanceUtils.java
@@ -9,6 +9,7 @@ import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import com.lsnju.tpbase.log.DigestConstants;
 import com.lsnju.tpbase.log.TpTraceInterceptor;
@@ -25,8 +26,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TpPerformanceUtils {
 
-    public static <T> PerfResult perfTest(List<List<T>> groupTask, Consumer<T> consumer) throws Exception {
-        final int threadSize = groupTask.size();
+    public static <T> PerfResult perfTest(List<T> allTask, int groupSize, Consumer<T> consumer) throws Exception {
+        if (allTask.size() < groupSize) {
+            throw new IllegalArgumentException("groupSize is greater than task");
+        }
+        List<List<T>> groupedTask = IntStream.range(0, groupSize)
+            .mapToObj(x -> new ArrayList<T>()).collect(Collectors.toList());
+        for (int i = 0; i < allTask.size(); i++) {
+            int groupNo = i % groupSize;
+            groupedTask.get(groupNo).add(allTask.get(i));
+        }
+        return perfTest(groupedTask, consumer);
+    }
+
+    public static <T> PerfResult perfTest(List<List<T>> groupedTask, Consumer<T> consumer) throws Exception {
+        final int threadSize = groupedTask.size();
         final ExecutorService executor = Executors.newFixedThreadPool(threadSize);
         final CountDownLatch ready = new CountDownLatch(threadSize);
         final CountDownLatch start = new CountDownLatch(1);
@@ -34,7 +48,7 @@ public class TpPerformanceUtils {
 
         List<CompletableFuture<PerfResult>> allFuture = new ArrayList<>();
         for (int i = 0; i < threadSize; i++) {
-            final List<T> jobList = groupTask.get(i);
+            final List<T> jobList = groupedTask.get(i);
             final String name = String.format("task_%02d", i);
             Supplier<PerfResult> supplier = TpTraceInterceptor.DEFAULT.supplier(name, call(name, jobList, consumer, ready, start, done));
             allFuture.add(CompletableFuture.supplyAsync(supplier, executor));
@@ -49,7 +63,7 @@ public class TpPerformanceUtils {
         List<PerfResult> subList = allFuture.stream().map(CompletableFuture::join).collect(Collectors.toList());
         final PerfResult ret = new PerfResult();
         ret.setSubList(subList);
-        ret.setTotalCount(groupTask.stream().mapToInt(List::size).sum());
+        ret.setTotalCount(groupedTask.stream().mapToInt(List::size).sum());
         ret.setTotalCost(totalCost);
         //
         executor.shutdown();
