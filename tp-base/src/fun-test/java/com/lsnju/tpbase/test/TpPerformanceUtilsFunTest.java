2480fc58cc3c97e032fbbaf2f56e3727aaac0424
lisong <ls_demon@qq.com>
2025-11-15 21:54:01 +0800

    polishing

--- tp-base/src/fun-test/java/com/lsnju/tpbase/test/TpPerformanceUtilsFunTest.java
+++ tp-base/src/fun-test/java/com/lsnju/tpbase/test/TpPerformanceUtilsFunTest.java
@@ -52,4 +52,35 @@ public class TpPerformanceUtilsFunTest {
         }
     }
 
+    @Test
+    void test_002() {
+        TpTraceInterceptor.DEFAULT.run("x", () -> {
+            try {
+                test002();
+            } catch (Exception e) {
+                log.error(String.format("%s", e.getMessage()), e);
+            }
+        });
+    }
+
+    private static void test002() throws Exception {
+        List<String> allTask = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            allTask.add(String.format("task_%03d", i));
+        }
+        PerfResult result = TpPerformanceUtils.perfTest(allTask, 4, s -> {
+            //
+            try {
+                log.info("{}", s);
+                Thread.sleep(RandomUtils.insecure().randomInt(0, 1000));
+            } catch (InterruptedException e) {
+                log.error(String.format("%s", e.getMessage()), e);
+            }
+        });
+        log.info("{}, {}, avg={}", result.getTotalCost(), result.getTotalCount(), result.avg());
+        for (PerfResult item : result.getSubList()) {
+            log.info("{}, avg={}", item, item.avg());
+        }
+    }
+
 }
