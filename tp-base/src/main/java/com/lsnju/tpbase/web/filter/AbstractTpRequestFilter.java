cda42f3f501f59975f998bb76cf5a2b2cbc78102
lisong <ls_demon@qq.com>
2025-11-22 17:05:35 +0800

    v4: polishing

--- tp-base/src/main/java/com/lsnju/tpbase/web/filter/AbstractTpRequestFilter.java
+++ tp-base/src/main/java/com/lsnju/tpbase/web/filter/AbstractTpRequestFilter.java
@@ -1,7 +1,6 @@
 package com.lsnju.tpbase.web.filter;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 import jakarta.servlet.ServletException;
@@ -11,14 +10,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.servlet.filter.OrderedFilter;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.lsnju.base.enums.BizErrEnum;
 import com.lsnju.base.model.rs.BaseResp;
 import com.lsnju.base.util.JsonUtils;
+import com.lsnju.tpbase.web.util.TpServletRespUtils;
 
 /**
  *
@@ -40,10 +38,7 @@ public abstract class AbstractTpRequestFilter extends OncePerRequestFilter imple
     }
 
     protected void errorJsonMsg(HttpServletResponse resp, HttpStatus httpStatus, String msgBody) throws IOException {
-        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        resp.getWriter().write(msgBody);
-        resp.setStatus(httpStatus.value());
+        TpServletRespUtils.errorJsonMsg(resp, httpStatus, msgBody);
     }
 
     protected void errorJsonMsg(HttpServletResponse resp, HttpStatus httpStatus, BizErrEnum bizError) throws IOException {
