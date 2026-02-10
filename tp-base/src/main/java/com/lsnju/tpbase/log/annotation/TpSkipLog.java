20a2621a12edaec70e0a21140dbd783f06e0b3d6
lisong <ls_demon@qq.com>
2026-02-10 22:40:16 +0800

    polishing

--- tp-base/src/main/java/com/lsnju/tpbase/log/annotation/TpSkipLog.java
+++ tp-base/src/main/java/com/lsnju/tpbase/log/annotation/TpSkipLog.java
@@ -1,10 +1,21 @@
 package com.lsnju.tpbase.log.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  *
  * @author lisong
  * @since 2026-02-10 21:34
  * @version V1.0
  */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
 public @interface TpSkipLog {
 }
