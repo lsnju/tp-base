5f7395739fbfe6132b7b092ec3d585cd061cc9e8
lisong <ls_demon@qq.com>
2026-03-18 20:15:30 +0800

    add http5

---
+++ tp-utils/src/main/java/com/lsnju/base/http5/Http5ExecutorUtils.java
@@ -0,0 +1,16 @@
+package com.lsnju.base.http5;
+
+import com.lsnju.base.http.HttpExecutorUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *
+ * @author lisong
+ * @since 2021/9/8 15:40
+ * @version V1.0
+ */
+@Slf4j
+public class Http5ExecutorUtils extends HttpExecutorUtils {
+
+}
