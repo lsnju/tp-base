38472e4a840b68046d7b566b66dd350bee16b520
lisong <ls_demon@qq.com>
2026-03-28 11:28:45 +0800

    polishing

--- tp-utils/src/main/java/com/lsnju/base/jackson/JacksonUtils.java
+++ tp-utils/src/main/java/com/lsnju/base/jackson/JacksonUtils.java
@@ -17,10 +17,13 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.StreamReadFeature;
+import com.fasterxml.jackson.core.StreamWriteFeature;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.util.StdDateFormat;
 import com.lsnju.base.money.Money;
@@ -36,32 +39,37 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class JacksonUtils {
 
-    public static final ObjectMapper DEFAULT_MAPPER = new ObjectMapper();
-    public static final ObjectMapper PRETTY_MAPPER = new ObjectMapper();
+    public static final JsonMapper DEFAULT_MAPPER;
+    public static final JsonMapper PRETTY_MAPPER;
     public static boolean WITH_JSR310 = ClazzUtils.exist("com.fasterxml.jackson.datatype.jsr310.JavaTimeModule");
     public static final TypeReference<Map<String, String>> MAP_TYPE_REFERENCE = new TypeReference<Map<String, String>>() {};
 
     static {
-        SimpleModule module = getDefaultModule();
-
         StdDateFormat dateFormat = new StdDateFormat()
             .withColonInTimeZone(true)
             .withLenient(true)
             .withTimeZone(TimeZone.getDefault());
 
-        PRETTY_MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        PRETTY_MAPPER.configure(SerializationFeature.INDENT_OUTPUT, true);
-        PRETTY_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        PRETTY_MAPPER.registerModule(module);
-        PRETTY_MAPPER.setDateFormat(dateFormat);
+        DEFAULT_MAPPER = JsonMapper.builder()
+            // write
+            .serializationInclusion(JsonInclude.Include.NON_NULL)
+            .disable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY)
+            .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+            .enable(SerializationFeature.WRITE_SELF_REFERENCES_AS_NULL)
+            .enable(StreamWriteFeature.IGNORE_UNKNOWN)
+            // read
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .enable(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION)
+            .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL)
+            // add module
+            .addModule(getDefaultModule())
+            .defaultDateFormat(dateFormat)
+            .build();
 
-        DEFAULT_MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        DEFAULT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        DEFAULT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        DEFAULT_MAPPER.enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL);
-        DEFAULT_MAPPER.registerModule(module);
-        DEFAULT_MAPPER.setDateFormat(dateFormat);
-        // MAPPER.registerModule(new JaxbAnnotationModule());
+        PRETTY_MAPPER = DEFAULT_MAPPER.rebuild()
+            // write
+            .enable(SerializationFeature.INDENT_OUTPUT)
+            .build();
     }
 
     public static SimpleModule getDefaultModule() {
