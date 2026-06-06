ef1b2d2885d13dbea6913e0fed1dbf2a42b63128
lisong <ls_demon@qq.com>
2026-06-06 18:57:14 +0800

    polishing

---
+++ tp-utils/src/main/java/com/lsnju/base/http/TpHttpConstants.java
@@ -0,0 +1,14 @@
+package com.lsnju.base.http;
+
+/**
+ *
+ * @author lis614
+ * @since 2026-06-06 18:52
+ * @version V1.0
+ */
+public interface TpHttpConstants {
+
+    String LOG_NAME = "HTTP_HEADER";
+    String REQ_ID = "TP_HTTP_ID";
+
+}
