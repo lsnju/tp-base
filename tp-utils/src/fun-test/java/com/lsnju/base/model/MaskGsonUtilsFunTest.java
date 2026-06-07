d1ed017559bbae2ca37db487f7b23bdae2d3ade6
lisong <ls_demon@qq.com>
2026-06-07 09:24:09 +0800

    add MaskGsonUtils

---
+++ tp-utils/src/fun-test/java/com/lsnju/base/model/MaskGsonUtilsFunTest.java
@@ -0,0 +1,45 @@
+package com.lsnju.base.model;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+import com.lsnju.base.model.vo.MaskVo;
+import com.lsnju.base.money.Money;
+import com.lsnju.base.util.JsonUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *
+ * @author lis614
+ * @since 2026-06-07 08:56
+ * @version V1.0
+ */
+@Slf4j
+public class MaskGsonUtilsFunTest {
+
+    @Test
+    void test_001() {
+        MaskVo v = new MaskVo();
+        v.setId(22);
+        v.setName("name");
+        v.setAmount(new Money(11.11));
+        v.setDate1(new Date());
+        v.setDate2(new Date());
+        v.setMemo("123456199901011234");
+        v.setPhone("123456199901011234");
+        v.setGid("123456199901011234");
+
+        v.setDesc1("123456199901011234");
+        v.setDesc2("123456199901011234");
+        v.setDesc3("123456199901011234");
+        v.setDesc4("123456199901011234");
+        v.setDesc5("123456199901011234");
+        v.setDesc6("123456199901011234");
+        String json = MaskGsonUtils.toJson(v);
+        log.info("{}", json);
+        log.info("{}", JsonUtils.toPrettyFormat(json));
+    }
+
+}
